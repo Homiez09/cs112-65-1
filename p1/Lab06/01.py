18b913bc53bc597bdce1e58c176a3a50978bd7da
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2022-08-09 16:39:52 +0700

    Update Lab06/01.py

--- p1/Lab06/01.py
+++ p1/Lab06/01.py
@@ -1,14 +1,48 @@
-target = 1234
-i = 0
-count = 0
-n = input("Enter your guess (4-digit integer): ")
+target = int(input("Enter a target (4-digit integer): "))
+guess = int(input("Enter your guess (4-digit integer): "))
+positionCount = 0
+digitCount = 0
+ans = ""
 
-if n == target:
-    print("Congratulations, you just mastered my mind!!")
+if target == guess:
+  print("Congratulations, you just mastered my mind!!")
 else:
-    while i < 4:
-        if n % 4 in target:
-            count += 1
-        i += 1
+  # check position
+  i = target
+  j = guess
+  while not i == 0:
+    if i % 10 == j % 10:
+      positionCount += 1
+    i //= 10
+    j //= 10
 
-print(count)
+  # check digit
+  i = target
+  while not i == 0:
+    j = guess
+    while not j == 0:
+      if i % 10 == j % 10:
+        digitCount += 1
+      j //= 10
+    i //= 10
+
+  if positionCount == 0:
+    ans += "No positions correct, "
+  elif positionCount == 1:
+    ans += "One position correct, " 
+  elif positionCount == 2:
+    ans += "Two positions correct, "
+  elif positionCount == 3:
+    ans += "Three positions correct, " 
+  digitCount -= positionCount
+  if digitCount == 0:
+    ans += "no digits correct"
+  elif digitCount == 1:
+    ans += "one digit correct"
+  elif digitCount == 2:
+    ans += "two digits correct"
+  elif digitCount == 3:
+    ans += "three digits correct"
+  elif digitCount == 4:
+    ans += "four digits correct"
+print(ans)
