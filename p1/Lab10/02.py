09382dcc4ac4eed27aabc4bfac3d3f5a9f611938
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2022-09-08 20:28:08 +0700

    Update

--- p1/Lab10/02.py
+++ p1/Lab10/02.py
@@ -10,10 +10,15 @@ while True:
     n = int(input("Enter a number (0 to end): "))
     if n == 0:
         break
-    elif n < 1 or n > 999:
+    elif n >= 1 and n <= 999:
+        number.append(n)
+    else:
         print("Number is out of range.")
         n = int(input("Enter a number (0 to end): "))
-    number.append(n)
+        if n == 0:
+            break
+        number.append(n)
+
 print(f"Original list:\n{number}")
 print("Accumulative Sum:")
 accum_sum(number)
---
+++ p1/Lab10/03.py
@@ -0,0 +1,28 @@
+import math 
+
+score = []
+def find_sd(l):
+    xBar = sum(l) / len(l)
+    sd = 0
+    for i in l:
+        sd += (i - xBar) ** 2
+    sd = sd / (len(l)-1)
+    return math.sqrt(sd)
+
+while True:
+    n = float(input("Enter score: "))
+    if n == -1:
+        break 
+    if n >= 0 and n <= 100:
+        score.append(n)
+    else:
+        print("Score is out of range.")
+        n = float(input("Enter score: "))
+        if n == -1:
+            break
+        score.append(n)
+if score:
+    print("Maximum score is {:.2f}.".format(max(score)))
+    print("Minimum score is {:.2f}.".format(min(score)))
+    print("Average score is {:.2f}.".format(sum(score) / len(score)))
+    print("Standard deviation is {:.2f}.".format(find_sd(score)))
