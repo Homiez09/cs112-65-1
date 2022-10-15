7cfe0529baf75b0f476ffe9b92129106d4052fcc
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2022-10-16 01:04:19 +0700

    Lab14-Done

--- Lab14.2/03.py
+++ Lab14.2/03.py
@@ -1,35 +1,21 @@
-def translate(ls: list):
-    ls_dict = {}
-    for i in ls:
-        x = i.split("=")
-        ls_dict[x[0].strip()] = int(x[1].strip())
-    return ls_dict
-    
-def export(var: list, code: list):
-    var = translate(var)
-    result = []
-    for i in code: 
-        content = i
-
-        for j in var:
-            if j in content: 
-                content = content.replace(f"{chr(123)}{j}{chr(125)}", str(var[j]))
-        result.append(content)
-    return result
-            
-if __name__ == "__main__":
-    print("Enter variables and values:")
-    var = []
-    while True:
-        var.append(input())
-        if var[-1] == "-1":
-            break
-
-    print("Enter your program:")
-    code = []
-    while True:
-        code.append(input())
-        if code[-1] == "-1":
-            break
-    print("Result:")
-    print(*export(var[:-1], code[:-1]), sep="\n")
+value = {}
+result = []
+print('Enter variables and values:')
+while True:
+    i = input()
+    if i == "-1":
+        break
+    x = i.split("=")
+    value[x[0].strip()] = x[1].strip()
+print('Enter your program:')
+print(value)
+while True:
+    s = input()
+    if s=='-1': 
+        break
+    for i in value:
+        s = s.replace('{'+i+'}',str(value[i]))
+    result.append(s)
+print('Result:')
+for i in result: 
+    print(i)
