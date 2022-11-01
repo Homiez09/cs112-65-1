40904499b1805ef5810deb8aa00719d4d9a7fbc8
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2022-11-02 04:03:34 +0700

    Final

---
+++ Lab14.3_Final/13.py
@@ -0,0 +1,9 @@
+n = int(input())
+digit = ['one','two','three','four','five','six','seven','eight','nine']
+teen = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+ty = ['', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+if not n > 999:
+    ...
+else:
+    print('ERROR')
